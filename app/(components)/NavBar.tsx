3a44f693bd653380e26772a2cc124f10448253aa
Nainglynn <nainglynnxd@gmail.com>
2025-08-21 22:35:04 +0100

    refactor: remove unnecessary css codes and center align the hero and navigation links

--- app/(components)/NavBar.tsx
+++ app/(components)/NavBar.tsx
@@ -33,11 +33,11 @@ export function NavBar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between px-4 sm:px-8 lg:px-12">
+      <div className="mx-auto container flex h-16 items-center sm:px-8">
         <div className="flex items-center gap-2 py-2">
-          <span className="font-bold text-xl p-2">PrepMaster</span>
+          <span className="font-bold text-xl py-2">PrepMaster</span>
         </div>
-        <nav className="hidden md:flex gap-8 mx-4">
+        <nav className="hidden md:flex gap-8 mx-auto">
           <Link
             href="#features"
             className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground p-2"
