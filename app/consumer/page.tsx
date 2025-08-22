00e2d2770a38f9bd7fd2a17fa974f92309beac1d
Nainglynn <nainglynnxd@gmail.com>
2025-08-22 17:23:19 +0100

    testing biome ci

--- app/consumer/page.tsx
+++ app/consumer/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { ChefHat } from "lucide-react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const navLinks = [
   { href: "#menu", label: "Menu" },
