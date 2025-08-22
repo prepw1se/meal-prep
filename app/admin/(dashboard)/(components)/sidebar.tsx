42e9806c3973d7e2610ec9ef68e36990cd641fab
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-08-22 11:50:14 +0100

    Refactor AdminSidebarWrapper layout for improved responsiveness and structure to stop shrinking the main page when opening drop down menu

--- app/admin/(dashboard)/(components)/sidebar.tsx
+++ app/admin/(dashboard)/(components)/sidebar.tsx
@@ -156,11 +156,11 @@ export function AdminSidebarWrapper({ children }: { children: React.ReactNode })
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="flex flex-1 flex-col">
+      <main className="w-full min-w-0">
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+        <div className="w-full min-h-screen p-4">{children}</div>
       </main>
     </SidebarProvider>
   );
